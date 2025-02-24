49d83c377fd5a3836f75beb323d3a6074acd7969
Ethan Kidwell <90011750+EthanK14@users.noreply.github.com>
2025-02-24 07:56:57 -0500

    adding POST features so we can upload to mongoDB database.

---
+++ Thrive/Backend/notesRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const database = require("./connect")
+
+let postRoutes = express.Router()
+
+// upload doc data in DB
+postRoutes.route("/upload").post(async (reqeust, response) => {
+    let db = database.getDB()
+
+    let mongoObject = {
+        meetingid: reqeust.body.meetingid,
+        client:reqeust.body.client,
+        author:reqeust.body.author,
+        participants:reqeust.body.participants,
+        date:reqeust.body.date,
+        notes: extractedText,
+    }
+    let data = await db.collection("MeetingNotes").insertOne(mongoObject)
+    
+    response.json(data)
+    
+})
+
+module.exports = postRoutes
