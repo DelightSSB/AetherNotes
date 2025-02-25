8ca1afc95bd05ab13ad476e06e99a5afa778ed41
Brooke Lavelle <brookelavelle@Brookes-MacBook-Pro.local>
2025-02-25 14:43:58 -0500

    Stylized Upload Button

--- Thrive/App.js
+++ Thrive/App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
 import * as DocumentPicker from 'expo-document-picker';
 import axios from 'axios';
+
+const logo = require('./assets/thrivelogo.png');
 
 //const fileUpload = require("./components/fileUpload")
 
@@ -42,16 +44,16 @@ function UploadButton() {
     }
   };
   return (
-    <>
-      <Button title="Upload a File" onPress={handleFileUpload} />
-      {fileInfo && <Text>Uploaded: {fileInfo.author}</Text>}
-    </>
+    <TouchableOpacity style={styles.uploadButton} onPress={handleFileUpload}>
+      <Text style={styles.uploadButtonText}>UPLOAD A FILE</Text>
+    </TouchableOpacity>
   );
 }
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image source={logo} style={styles.logo} />
       <UploadButton />
       <StatusBar style="auto" />
     </View>
@@ -63,6 +65,33 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingTop: 50,
+    paddingBottom: 20,
+  },
+  logo: {
+    width: 200,
+    height: 80,
+    resizeMode: 'contain',
+    marginBottom: 20,
+  },
+  uploadButton:{
+    position: 'absolute',
+    bottom: 30,
+    left: "50%",
+    transform: [{ translateX: -75 }],
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    backgroundColor: "#fff",
+    borderWidth: 1,
+    borderColor: "#000",
+    borderRadius: 5, 
+    alignItems: 'center',
     justifyContent: 'center',
+  },
+  uploadButtonText:{
+    color: "#000",
+    fontSize: 12,
+    fontWeight: "bold",
   },
 });
