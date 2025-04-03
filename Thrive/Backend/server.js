e41ec3abbc28df6762e5b0794a67deaa49b4b8f4
Ethan Kidwell <90011750+EthanK14@users.noreply.github.com>
2025-04-03 17:29:09 -0400

    edited server.js file

--- Thrive/Backend/server.js
+++ Thrive/Backend/server.js
@@ -3,6 +3,7 @@ const express = require("express")
 const cors = require("cors")
 const upload = require("./uploadRoute")
 const summary = require("./summaryRoute")
+const prompt = require("./promptRoute")
 
 const app = express()
 const PORT = 3000
@@ -11,6 +12,7 @@ app.use(cors())
 app.use(express.json({limit: '150mb'}))
 app.use(upload)
 app.use(summary)
+app.use(prompt)
 
 app.listen(PORT, () => {
     connect.connectToServer()
